7223d48be6a48ca9a73680ec13900775317f387c
nsahloum <sahloumi.najima@gmail.com>
2021-12-23 21:01:16 +0100

    starting spring boot project

---
+++ src/main/java/com/example/tempapiboot/TempapibootApplication.java
@@ -0,0 +1,13 @@
+package com.example.tempapiboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempapibootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempapibootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tempapiboot/TempapibootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tempapiboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempapibootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
